6771d62172a1b5545567e6c2baa4c13679744335
Victor ROBERT LAMBRECHT <victor.robert-lam@cs-student.fr>
2024-05-01 12:05:56 +0200

    modele 1.3

--- run.py
+++ run.py
@@ -223,42 +223,38 @@ experimental_dataset = VideoDataset(dataset_dir, dataset_choice="experimental", 
 class CNNVideoClassifier(nn.Module):
     def __init__(self, num_classes):
         super(CNNVideoClassifier, self).__init__()
-        self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn1 = nn.BatchNorm3d(64)
-        self.conv2 = nn.Conv3d(64, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn2 = nn.BatchNorm3d(64)
-        self.conv3 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn3 = nn.BatchNorm3d(128)
-        self.conv4 = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn4 = nn.BatchNorm3d(256)
-        self.conv5 = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn5 = nn.BatchNorm3d(256)
-        # Other layers unchanged for simplicity
-        self.pool = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
-        self.fc1 = nn.Linear(512 * 4 * 4, 2048)
-        self.fc2 = nn.Linear(2048, 2048)
-        self.fc3 = nn.Linear(2048, num_classes)
+        self.conv1 = nn.Conv3d(3, 16, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1))
+        self.bn1 = nn.BatchNorm3d(16)
+        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        
+        self.conv2 = nn.Conv3d(16, 32, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1))
+        self.bn2 = nn.BatchNorm3d(32)
+        self.pool2 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        
+        self.conv3 = nn.Conv3d(32, 64, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1))
+        self.bn3 = nn.BatchNorm3d(64)
+        self.pool3 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+
+        # The output from the last pooling layer will have dimensions [batch_size, 64, 10, 32, 32]
+        self.fc1 = nn.Linear(64 * 10 * 32 * 32, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
         x = self.relu(self.bn1(self.conv1(x)))
-        x = self.pool(x)
+        x = self.pool1(x)
         x = self.relu(self.bn2(self.conv2(x)))
-        x = self.pool(x)
+        x = self.pool2(x)
         x = self.relu(self.bn3(self.conv3(x)))
-        x = self.pool(x)
-        x = self.relu(self.bn4(self.conv4(x)))
-        x = self.pool(x)
-        x = self.relu(self.bn5(self.conv5(x)))
-        x = self.pool(x)
-        x = x.view(-1, 512 * 4 * 4)
-        x = self.dropout(x)
+        x = self.pool3(x)
+        
+        # Flatten the output for the fully connected layer
+        x = x.view(x.size(0), -1)  # Flatten preserving the batch dimension
         x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
+
 
 # Example usage:
 # Define your video classification model
